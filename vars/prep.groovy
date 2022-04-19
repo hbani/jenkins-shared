227af2606140aab3c12dd5b1ed53b83bcb39f71a
hbani <74787075+hbani@users.noreply.github.com>
2022-04-20 00:45:15 +0200

    Update prep.groovy

--- vars/prep.groovy
+++ vars/prep.groovy
@@ -14,7 +14,7 @@ def save_conf_client(client_name,config) {
     getconfig.config.put(e.key,e.value)
     }
   println(getconfig.getClass())
-  writeYaml(file: "${WORKSPACE}/${client_name}.yaml",datas: getconfig)
+  writeYaml(file: "${WORKSPACE}/${client_name}.yaml",data: getconfig,overwrite: true)
   def clientYaml = readYaml(file: "${WORKSPACE}/${client_name}.yaml")
   return clientYaml
 }
