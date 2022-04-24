b34cab1c17f7b8730da7e4371e58953b412a055c
hbani <74787075+hbani@users.noreply.github.com>
2022-04-24 17:14:29 +0200

    Update prep.groovy

--- vars/prep.groovy
+++ vars/prep.groovy
@@ -9,7 +9,7 @@ def get_conf_client(client_name) {
 }
 
 def save_conf_client(client_name,config) {
-  Object getconfig = get_conf_client(client_name)
+  Map getconfig = get_conf_client(client_name)
   for ( e in config ) {
     getconfig.config.put(e.key,e.value)
     }
