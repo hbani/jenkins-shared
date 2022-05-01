bd749bb904021daa725d70c5be6b8d552f4a8b94
hbani <74787075+hbani@users.noreply.github.com>
2022-05-01 18:36:19 +0200

    Update prep.groovy

--- vars/prep.groovy
+++ vars/prep.groovy
@@ -8,6 +8,18 @@ def get_conf_client(client_name) {
   return conf."$client_name"
 }
 
+def save_conf_client(client_name,config) {
+  Map getconfig = get_conf_client(client_name)
+  for ( e in config ) {
+    getconfig.config.put(e.key,e.value)
+    }
+  println(getconfig)
+  println(getconfig.getClass())
+  writeYaml(file: "${WORKSPACE}/${client_name}.yaml",data: getconfig,returnText: true)
+  def clientYaml = readYaml(file: "${WORKSPACE}/${client_name}.yaml")
+  return clientYaml
+}
+
 def get_aws_cred() {
   return readJSON(text: sh(script: "curl http://169.254.169.254/latest/meta-data/iam/security-credentials/mgmt-eu-west-2-prod-ci-slave-backtest", returnStdout: true))
 }
@@ -16,18 +28,18 @@ def config_download(client_name) {
   def awsCredentials = get_aws_cred()
   dir ("config") {
       def conf = get_conf_client(client_name)
-      constants.configRoot = pwd()
+      configRoot = pwd()
 
-      common.downloadFileFromS3(awsCredentials, "s3://compass-simulations-config/"+conf.config.bootstrapProperties, constants.configRoot)
+      common.downloadFileFromS3(awsCredentials, "s3://compass-simulations-config/"+conf.config.bootstrapProperties, configRoot)
 
-      constants.hedgingConfiguration = common.downloadFileFromS3(awsCredentials,  "s3://compass-simulations-config/"+conf.config.configurationS3UrlPrefix, constants.configRoot)
+      constants.hedgingConfiguration = common.downloadFileFromS3(awsCredentials,  "s3://compass-simulations-config/"+conf.config.configurationS3UrlPrefix, configRoot)
   }
-  println constants.hedgingConfiguration
+  save_conf_client('hfmarkets',[configRoot: configRoot, hedgingConfiguration: hedgingConfiguration])
 }
 
 def create_output_path() {
   dir ("output") {
-      constants.outputPath = pwd()
+      outputPath = pwd()
 
       // Create the damn folder
       writeFile file:'dummy', text:''
