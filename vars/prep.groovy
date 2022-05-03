138f1bb7968586296b13cda45f14930ffdfa6461
hbani <74787075+hbani@users.noreply.github.com>
2022-05-03 22:50:55 +0200

    Update prep.groovy

--- vars/prep.groovy
+++ vars/prep.groovy
@@ -2,6 +2,9 @@ import org.yaml.snakeyaml.Yaml
 
 def get_conf_client(client_name) {
   if ( fileExists("${WORKSPACE}/${client_name}.yaml") ) {
+  sh  """
+  rm -rf "${WORKSPACE}/${client_name}.yaml"
+  """
     def clientYaml = readYaml(file: "${WORKSPACE}/${client_name}.yaml")
     return clientYaml
   }
