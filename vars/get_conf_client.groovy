94fbabebdcdab7cfd752a3283258cab8b3099915
hbani <74787075+hbani@users.noreply.github.com>
2022-05-08 17:18:08 +0200

    adding validation

--- vars/get_conf_client.groovy
+++ vars/get_conf_client.groovy
@@ -3,7 +3,8 @@ def call(client_name) {
     def clientYaml = readYaml(file: "${WORKSPACE}/${client_name}.yaml")
     return clientYaml
   }
-  def yamlString = libraryResource('clients/config/clients.yaml')
-  Map conf = readYaml(text: yamlString)
+  def file = new File("${WORKSPACE}/jenkins\config/analytics/jenkinsfiles/customer-simulation-client.yaml")
+  assert file.exists() : "Clients config file not found"
+  def conf = readYaml(file:'${WORKSPACE}/jenkins\config/analytics/jenkinsfiles/customer-simulation-client.yaml')
   return conf."$client_name"
 }
