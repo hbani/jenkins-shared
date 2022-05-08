bb4cb104cd9d36c6eda4534d737ad2abae55808a
hbani <74787075+hbani@users.noreply.github.com>
2022-05-08 17:27:15 +0200

    Update get_conf_client.groovy

--- vars/get_conf_client.groovy
+++ vars/get_conf_client.groovy
@@ -3,7 +3,7 @@ def call(client_name) {
     def clientYaml = readYaml(file: "${WORKSPACE}/${client_name}.yaml")
     return clientYaml
   }
-  def file = new File("${WORKSPACE}/jenkins\config/analytics/jenkinsfiles/customer-simulation-client.yaml")
+  def file = new File("${WORKSPACE}/jenkins/config/analytics/jenkinsfiles/customer-simulation-client.yaml")
   assert file.exists() : "Clients config file not found"
   def conf = readYaml(file:'${WORKSPACE}/jenkins/config/analytics/jenkinsfiles/customer-simulation-client.yaml')
   return conf."$client_name"
