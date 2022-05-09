3572cfcfe42c0136ee0b2ab7055885328509a31d
hbani <74787075+hbani@users.noreply.github.com>
2022-05-09 21:26:12 +0200

    remove unused code

--- vars/downloadFileFromS3.groovy
+++ vars/downloadFileFromS3.groovy
@@ -1,14 +1,5 @@
 def call(awsCredentials, s3Url, localDir) {
-// TODO: Need Jenkins 2.60.3 for https://plugins.jenkins.io/pipeline-aws :(
-//         withAws(credentials:'tickdata-s3-readonly') {
-//            s3Download(file:'/', bucket:'compass-simulations-config', path:configRoot, force:true)
-//         }
-
     s3Url = s3Url.replaceAll(/^https?:\/\/s3-[^\.\/]+\.amazonaws\.com\/([^\/]+)\//, 's3://$1/')
-
-//   withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'JenkinsBacktestSlave', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
     sh "s3cmd --verbose --preserve --access_key=${awsCredentials.AccessKeyId} --secret_key=\"${awsCredentials.SecretAccessKey}\" --access_token=\"${awsCredentials.Token}\" --force get ${s3Url} ${localDir}"
-//   }
-
    return new File(localDir, s3Url.substring(s3Url.lastIndexOf('/') + 1));
 }
--- vars/shellString.groovy
+++ vars/shellString.groovy
@@ -2,11 +2,5 @@ def call(s) {
   if (s.trim() == '') {
       return '';
   }
-
-// Replace ' with '\'' (https://unix.stackexchange.com/a/187654/260156). Then enclose with '...'.
-// 1) Why not replace \ with \\? Because '...' does not treat backslashes in a special way.
-// 2) And why not use ANSI-C quoting? I.e. we could replace ' with \'
-// and enclose using $'...' (https://stackoverflow.com/a/8254156/4839573).
-// Because ANSI-C quoting is not yet supported by Dash (default shell in Ubuntu & Debian) (https://unix.stackexchange.com/a/371873).
 '\'' + s.replace('\'', '\'\\\'\'') + '\''
 }
