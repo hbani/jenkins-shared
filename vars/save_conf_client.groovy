795efe1fe116e9a06e47424eaa14f2b22ea02010
hbani <74787075+hbani@users.noreply.github.com>
2022-05-09 21:53:11 +0200

    Update save_conf_client.groovy

--- vars/save_conf_client.groovy
+++ vars/save_conf_client.groovy
@@ -1,7 +1,7 @@
 def call(client_name,config) {
   Map getconfig = get_conf_client(client_name)
   for ( e in config ) {
-    getconfig.config.put(e.key,"${e.value}")
+    getconfig.put(e.key,"${e.value}")
     }
   writeYaml(file: "${WORKSPACE}/${client_name}.yaml",data: getconfig,overwrite: true)
 }
