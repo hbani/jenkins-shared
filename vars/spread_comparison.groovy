ff5d8318dc5ac7c4a5f26eedcb5aa5c5ff46cc74
hbani <74787075+hbani@users.noreply.github.com>
2022-06-22 23:41:07 +0200

    Update spread_comparison.groovy

--- vars/spread_comparison.groovy
+++ vars/spread_comparison.groovy
@@ -4,9 +4,9 @@ def call(client_name) {
 def conf = get_conf_client(client_name,"SpreadComparison")
 
 default_params = get_class_params('javaDefault',conf)
+print default_params
 SpreadComparison = get_class_params('SpreadComparison',conf)
 
-print default_params
 print SpreadComparison
 
 }
