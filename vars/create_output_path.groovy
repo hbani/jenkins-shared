de25a27865b162ae461a42aa752df3ee228f16be
hbani <74787075+hbani@users.noreply.github.com>
2022-06-22 23:09:49 +0200

    Update create_output_path.groovy

--- vars/create_output_path.groovy
+++ vars/create_output_path.groovy
@@ -1,9 +1,9 @@
-def call(client_name) {
+def call(client_name,classname) {
   dir ("output") {
       outputPath = pwd()
 
       // Create the damn folder
       writeFile file:'dummy', text:''
   }
-  save_conf_client(client_name,[outputPath: outputPath])
+  save_conf_client(client_name,classname,[outputPath: outputPath])
 }
