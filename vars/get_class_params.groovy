975ba6e98eb236737611b35258d1692fe072d4b4
hbani <74787075+hbani@users.noreply.github.com>
2022-06-22 23:15:41 +0200

    Update get_class_params.groovy

--- vars/get_class_params.groovy
+++ vars/get_class_params.groovy
@@ -3,7 +3,7 @@ import groovy.text.GStringTemplateEngine
 def call(name,Config) {
 return GStringTemplateEngine
       .newInstance()
-      .createTemplate(libraryResource('templates/${name}.params'))
+      .createTemplate(libraryResource("templates/${name}.params"))
       .make(config)
       .toString()
 }
