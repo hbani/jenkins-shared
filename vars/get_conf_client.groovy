72165025c52320341dbac5bc6b363e468e1567e0
hbani <74787075+hbani@users.noreply.github.com>
2022-06-22 22:33:25 +0200

    Update get_conf_client.groovy

--- vars/get_conf_client.groovy
+++ vars/get_conf_client.groovy
@@ -1,9 +1,9 @@
-def call(client_name,class) {
+def call(client_name, classname) {
   if ( fileExists("${WORKSPACE}/${client_name}.yaml") ) {
     def clientYaml = readYaml(file: "${WORKSPACE}/${client}.yaml")
     return clientYaml
   }
-  def yamlString = libraryResource('config/${class}.yaml')
+  def yamlString = libraryResource('config/${classname}.yaml')
   Object conf = readYaml(text: yamlString)
   return conf.defaults + conf."$client_name"
 }
