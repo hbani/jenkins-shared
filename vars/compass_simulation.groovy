bb0d0941a8155ae6f6cf85443bf2c6df063c529b
hbani <74787075+hbani@users.noreply.github.com>
2022-06-24 23:11:54 +0200

    add compass simulation

--- vars/compass_simulation.groovy
+++ vars/compass_simulation.groovy
@@ -1,59 +1,16 @@
 def call(client_name) {
-def conf = get_conf_client(client_name,"SpreadComparision")
-to_from = to_from()
-sh  """
-cd /app/fx/apps/mahifx/
 
-sed -i 's/arbHedger1/backtest/g' \"${conf.hedgingConfiguration}\"
-sed -i 's/hybridHedger1/backtest/g' \"${conf.hedgingConfiguration}\"
+  def conf = get_conf_client(client_name,"SpreadComparison")
+  config = [
+  "to_from": to_from(),
+  "shellString": shellString,
+  "env": env,
+  "params": params
+  ] + conf
 
-df -h
-free -m
-du -ms /tmp/*
+  default_params = get_class_params('javaDefault',config)
+  println default_params
 
-java \
-    -cp current/lib:current/lib/* \
-    -Duser.timezone=UTC \
-    -Xmx11G \
-    -Xms11G \
-    -XX:+UseParallelGC \
-    -XX:+PerfDisableSharedMem \
-    -XX:+HeapDumpOnOutOfMemoryError \
-    -DbuilderHelperFactory=com.x.integration.MahiSimulationProcessBuilderHelperFactory \
-    -DGraphPerformanceInstrumentation.PERFORMANCE_INSTRUMENTATION_MODE=Minimal \
-    -Dbootstrap.properties=file:${conf.configRoot}/${conf.bootstrapProperties} \
-    -Dlic.file=file:shared/conf/license.lic \
-    -DFileStorageProcessBuilderHelper.DataStore=/media/ephemeral0/tickstore/ \
-    -Droot.log.level=ERROR \
-    -Dmahi.root.log.level=INFO \
-    -Dbacktest.manage.clientTrades=true \
-    -Dbackstatsprovider.suppress.dump=true \
-    -Dbacktest.alternateProcessNames=${params.alternateProcessNames} \
-    -Dcompass.backtest=true \
-    -Dfile.encoding=UTF-8 \
-    ${conf.additionalSysprops} \
-    com.x.integration.analytics.CompassSimulationProcess \
-    --name=backtest \
-    --env=local-live,backtest \
-    --loadConfig=\"${conf.configurationS3Url}\" \
-    ${conf.fastMarkets}
-    --forceConfig=risk.varEquivalentRisk.maxVarLevel=double:${conf.maxVarLevel} \
-    --forceConfig=hedging.rules.aggressive.dynamicOrderSpeedScaling=double:${conf.dynamicOrderSpeedScaling} \
-    --forceConfig=analytics.enrichedTradeOutput.persist=boolean:false \
-    --forceConfig=analytics.enrichedTradeOutputSummary.persist=boolean:false \
-    --forceConfig=analytics.hedgingDecisions.persist=boolean:false \
-    ${conf.hybridInstrumentToProfileMappings}
-    --graphBuilderHelperFactory=com.x.processbuilder.SimulationGraphBuilderHelperFactory \
-    --jmsStoreUrl=\"${conf.jmsStoreUrl}\" \
-    --tickStoreUrl=\"${conf.tickStoreUrl}\" \
-    --outPath=\"${conf.outputPath}\" \
-    --client=\"${client_name}\" \
-    --jobId=\"${BUILD_TAG}\" \
-    --jobDescription=\"${params.goal}\" \
-    --from=\"${to_from.from}\" \
-    --to=\"${to_from.to}\" \
-    ${conf.additionalArgs} \
-    ${conf.pos} \
-
-"""
+  class_params = get_class_params('CompassSimulation',config)
+  println class_params
 }
--- vars/spread_comparison.groovy
+++ vars/spread_comparison.groovy
@@ -1,12 +1,10 @@
 import groovy.text.GStringTemplateEngine
 
 def call(client_name) {
-def conf = get_conf_client(client_name,"SpreadComparison")
+  def conf = get_conf_client(client_name,"SpreadComparison")
 
-default_params = get_class_params('javaDefault',conf)
-print default_params
-SpreadComparison = get_class_params('SpreadComparison',conf+['env':env,'params':params])
+  default_params = get_class_params('javaDefault',conf)
 
-print SpreadComparison
+  SpreadComparison = get_class_params('SpreadComparison',conf+['env':env,'params':params])
 
 }
