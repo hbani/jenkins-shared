a56992468cb2d02dc820371a6adb008d34d09d6d
hbani <74787075+hbani@users.noreply.github.com>
2022-06-25 23:21:12 +0200

    Update print_params.groovy

--- vars/print_params.groovy
+++ vars/print_params.groovy
@@ -5,8 +5,8 @@ def call(client_name,classname) {
   def conf = get_conf_client(client_name,classname)
   if (classname == "CompassSimulation") {
   to_from = to_from()
-  conf.put(from,to_from.from)
-  conf.put(to,to_from.to)
+  conf.put("from",to_from.from)
+  conf.put("to",to_from.to)
   }
   print prettyPrint(toJson(params))
 }
