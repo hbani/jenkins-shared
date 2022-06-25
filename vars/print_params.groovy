90c5f79a6359da76c63248aa4298c9dd840d0b3f
hbani <74787075+hbani@users.noreply.github.com>
2022-06-25 23:12:23 +0200

    Update print_params.groovy

--- vars/print_params.groovy
+++ vars/print_params.groovy
@@ -3,7 +3,7 @@ import static groovy.json.JsonOutput.*
 
 def call(client_name,classname) {
   def conf = get_conf_client(client_name,classname)
-  if classname == "CompassSimulation" {
+  if (classname == "CompassSimulation") {
   to_from = to_from()
   conf.put(from,to_from.from)
   conf.put(to,to_from.to)
