42e965d67f41cf1cc2accc9b858134864ff864b3
hbani <74787075+hbani@users.noreply.github.com>
2022-06-29 20:51:29 +0200

    Update spread_comparison.groovy

--- vars/spread_comparison.groovy
+++ vars/spread_comparison.groovy
@@ -3,7 +3,7 @@ import groovy.text.GStringTemplateEngine
 def call(client_name) {
   def conf = get_conf_client(client_name,"SpreadComparison")
 
-  default_params = get_class_params('javaDefault',conf)
+  default_params = get_class_params('javaDefault',conf+['env':env,'params':params])
 
   SpreadComparison = get_class_params('SpreadComparison',conf+['env':env,'params':params])
 
