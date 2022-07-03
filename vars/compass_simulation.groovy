78d49e17b6bea22cfe04d5e5aab6eb19a44b2364
hbani <74787075+hbani@users.noreply.github.com>
2022-07-03 15:49:57 +0200

    Update compass_simulation.groovy

--- vars/compass_simulation.groovy
+++ vars/compass_simulation.groovy
@@ -6,6 +6,20 @@ def call(client_name) {
 
   CompassSimulation = get_class_params('CompassSimulation',conf+['env':env,'params':params])
 
-  println default_params
-  println CompassSimulation
+  sh """
+  cd /app/fx/apps/mahifx/
+
+  sed -i 's/arbHedger1/backtest/g' \"${conf.hedgingConfiguration}\"
+  sed -i 's/hybridHedger1/backtest/g' \"${conf.hedgingConfiguration}\"
+
+  df -h
+  free -m
+  du -ms /tmp/*
+
+  java \
+    -cp current/lib:current/lib/* \
+    ${default_params} \
+    ${CompassSimulation}
+
+  """
 }
