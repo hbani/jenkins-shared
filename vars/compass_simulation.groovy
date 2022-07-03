5e6a47a07d4504791da9e0644ab77aaef4b7b6fe
hbani <74787075+hbani@users.noreply.github.com>
2022-07-03 13:37:14 +0200

    test

--- vars/compass_simulation.groovy
+++ vars/compass_simulation.groovy
@@ -6,20 +6,6 @@ def call(client_name) {
 
   CompassSimulation = get_class_params('CompassSimulation',conf+['env':env,'params':params])
 
-  sh """
-  cd /app/fx/apps/mahifx/
-
-  sed -i 's/arbHedger1/backtest/g' \"${conf.hedgingConfiguration}\"
-  sed -i 's/hybridHedger1/backtest/g' \"${conf.hedgingConfiguration}\"
-
-  df -h
-  free -m
-  du -ms /tmp/*
-
-  java \
-    -cp current/lib:current/lib/* \
-    ${default_params} \
-    ${CompassSimulation}
-
-  """
+  println default_params
+  println CompassSimulation
 }
--- vars/get_conf_client.groovy
+++ vars/get_conf_client.groovy
@@ -1,9 +1,19 @@
 def call(client_name, classname) {
+  Object conf = [:]
   if ( fileExists("${WORKSPACE}/${client_name}.yaml") ) {
     def clientYaml = readYaml(file: "${WORKSPACE}/${client_name}.yaml")
     return clientYaml
   }
-  def yamlString = libraryResource("config/${classname}.yaml")
-  Object conf = readYaml(text: yamlString)
-  return conf.defaults + conf."$client_name"
+  if ( libraryResource("config/${classname}.yaml" ) {
+    def yamlStringclass = libraryResource("config/${classname}.yaml")
+    Object confClass = readYaml(text: yamlStringclass)
+    if (conf.containsKey(client_name)) {
+      conf << conf."client_name"
+    }
+  }
+
+  def yamlStringclients = libraryResource("config/clients.yaml")
+  Object confClients = readYaml(text: yamlStringclients)
+  conf << confClass.defaults + conf."$client_name"
+  return conf
 }
