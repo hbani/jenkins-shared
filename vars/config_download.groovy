e7ea06bdb21fe75164c1b916993faedd3aff2907
hbani <74787075+hbani@users.noreply.github.com>
2022-07-03 16:08:57 +0200

    Update config_download.groovy

--- vars/config_download.groovy
+++ vars/config_download.groovy
@@ -8,7 +8,7 @@ def call(client_name,classname) {
 
       downloadFileFromS3(awsCredentials, "s3://compass-simulations-config/"+conf.bootstrapProperties, configRoot)
 
-      hedgingConfiguration = downloadFileFromS3(awsCredentials,  "${configurationS3Url}/${params.client_name.toUpperCase()}/${params.client_name.toUpperCase()}_config-update.json.gz", configRoot)
+      hedgingConfiguration = downloadFileFromS3(awsCredentials,  "${conf.configurationS3Url}/${params.client_name.toUpperCase()}/${params.client_name.toUpperCase()}_config-update.json.gz", configRoot)
       tempconfig.put("hedgingConfiguration",hedgingConfiguration)
   }
   save_conf_client(client_name,classname,tempconfig)
